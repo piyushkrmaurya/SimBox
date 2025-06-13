b785436768b56109d266552f84f16fcdfc7ffd81
Piyush Maurya <piyushkumarmaurya@gmail.com>
2025-06-13 09:35:56 +0530

    Docs: add 1 file(s) (64/100)

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="description" content="Interactive Simulations: Stop memorizing, Start Understanding!" />
+        <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <meta name="theme-color" content="#ff9933" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
